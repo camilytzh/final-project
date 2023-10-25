b7c32e1d3d40574ea26152521a17670e60086e7f
Camily <tctorres@espol.edu.ec>
2023-10-24 21:29:09 -0500

    Added methods definition for classes

--- src/model/UniversityClass.java
+++ src/model/UniversityClass.java
@@ -7,4 +7,29 @@ public class UniversityClass {
 	private String classroom;
 	private ArrayList <Student> lst_students;
 	private Teacher teacher;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getClassroom() {
+		return classroom;
+	}
+	public void setClassroom(String classroom) {
+		this.classroom = classroom;
+	}
+	public ArrayList<Student> getLst_students() {
+		return lst_students;
+	}
+	public void setLst_students(ArrayList<Student> lst_students) {
+		this.lst_students = lst_students;
+	}
+	public Teacher getTeacher() {
+		return teacher;
+	}
+	public void setTeacher(Teacher teacher) {
+		this.teacher = teacher;
+	}
+	
 }
