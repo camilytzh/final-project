44b00eaae204542d58e378001ac2425b1c1e4505
Camily <tctorres@espol.edu.ec>
2023-10-29 20:35:18 -0500

    Implemented loadUniversity with data and made main method menu

--- src/viewer/Main.java
+++ src/viewer/Main.java
@@ -1,13 +1,90 @@
 package viewer;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import controller.UniversityController;
 import model.Course;
 import model.Student;
 import model.Teacher;
+import persistence.DataInitializer;
 
 public class Main {
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		UniversityController univController = DataInitializer.loadUniversity();
 		
+		String generalMessage = "Welcome to Globant University!\n1. Show all professors\n2. Show all courses\n3. Create student\n4. Create course\n5. Show all courses by student id\n6. Exit";
+		String option = "10";
+		while(!option.equals("6"))
+		{
+			System.out.println(generalMessage);
+			System.out.print("Enter an option: ");
+			option = scanner.nextLine();
+			System.out.println("\n\n***************************\n\n");
+			if(option.equals("1"))
+			{
+				showAllTeachers(univController);
+			} else if(option.equals("2"))
+			{
+				showAllCourses(univController);
+				System.out.println("\n\n***************************\n\n");
+				System.out.println("To show all information of a course, please select the course option");
+				System.out.print("Enter one of the course options: ");
+				int optionForSubMenu = Integer.parseInt(scanner.nextLine());
+				System.out.println("\n\n***************************\n\n");
+				int courseSizes = univController.getUniversity().getCollegeCourses().size();
+				if(optionForSubMenu < 1 && optionForSubMenu >= courseSizes)
+				{
+					System.out.println("You selected a wrong course option :C");
+				} else
+				{
+					showCoursePeopleByOption(univController, optionForSubMenu);
+				}
+			} else if(option.equals("3"))
+			{
+				System.out.print("Please, enter the following data to register a student:\nName: ");
+				String studentName = scanner.nextLine();
+				System.out.print("Age: ");
+				int studentAge = Integer.parseInt(scanner.nextLine());
+				univController.createStudent(studentName, studentAge);
+			} else if (option.equals("4"))
+			{
+				System.out.print("Please, enter the following data to register a course:\nCourse Name: ");
+				String courseName = scanner.nextLine();
+				System.out.print("Classroom number: ");
+				int classroomNum = Integer.parseInt(scanner.nextLine());
+				univController.createCourse(courseName, classroomNum);
+			} else if(option.equals("5"))
+			{
+				System.out.println("To retrieve the classes that a student is enrolled, you have to enter the ID: ");
+				for(Student student: univController.getCollegeStudents())
+				{
+					System.out.println(student.getId());
+				}
+				System.out.print("Please enter a student id: ");
+				int studentId = Integer.parseInt(scanner.nextLine());
+				ArrayList<Course> coursesByStudent = univController.getCoursesByStudentId(studentId);
+				if(coursesByStudent.size() == 0)
+				{
+					System.out.println("The student neither exists nor is enrolled in a course");
+				} else
+				{
+					Student tmpStudent = univController.getStudentById(studentId);
+					System.out.println("The student: " + tmpStudent + "\nThe courses he is enrolled are: ");
+					for(Course course: coursesByStudent)
+					{
+						System.out.println(course);
+					}
+				}
+			} else if(!option.equals("6"))
+			{
+				System.out.println("Please, select a correct answer!");
+			}
+			System.out.println("\n\n***************************\n\n");
+		}
+		scanner.close();
+		System.out.println("Program finished, have a good day!");
 	}
 	
 	public static void showAllTeachers(UniversityController universityController)
@@ -20,9 +97,11 @@ public class Main {
 	
 	public static void showAllCourses(UniversityController universityController)
 	{
+		int subMenuOption = 1;
 		for(Course course: universityController.getUniversity().getCollegeCourses())
 		{
-			System.out.println(course);
+			System.out.println(subMenuOption + ". " + course);
+			subMenuOption += 1;
 		}
 	}
 	
